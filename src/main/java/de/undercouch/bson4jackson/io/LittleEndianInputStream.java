68379c98ebbb47335265dde0e38d6b1abbec15fb
Michel Kraemer <michel@undercouch.de>
2010-09-12 16:06:40 +0200

    Added LittleEndianInputStream that reads primitive values from input streams using little endian encoding

---
+++ src/main/java/de/undercouch/bson4jackson/io/LittleEndianInputStream.java
@@ -0,0 +1,330 @@
+package de.undercouch.bson4jackson.io;
+
+import java.io.DataInput;
+import java.io.DataInputStream;
+import java.io.EOFException;
+import java.io.FilterInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PushbackInputStream;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CoderResult;
+
+/**
+ * Works like {@link DataInputStream} but reads values using
+ * little-endian encoding. Apart from that, it provides a method
+ * that reads an UTF-8 encoded string without reading the number of
+ * bytes from the input stream.
+ * @author Michel Kraemer
+ */
+public class LittleEndianInputStream extends FilterInputStream implements DataInput {
+	/**
+	 * A small buffer to speed up reading slightly
+	 */
+	private byte[] _rawBuf;
+	
+	/**
+	 * Wraps around {@link #_rawBuf}
+	 */
+	private ByteBuffer _buf;
+	
+	/**
+	 * A buffer that will lazily be initialized by {@link #readLine()}
+	 */
+	private CharBuffer _lineBuffer;
+	
+	/**
+	 * A buffer that will lazily be initialized by {@link #readUTF(int)}
+	 */
+	private byte[] _rawUtf8Buf;
+	
+	/**
+	 * Wraps around {@link #_rawBuf}
+	 */
+	private ByteBuffer _utf8buf;
+	
+	/**
+	 * The character set used in {@link #readUTF(int)}. Will be
+	 * created lazily in {@link #getUTF8Charset()}
+	 */
+	private static Charset _utf8;
+	
+	/**
+	 * The decoder used in {@link #readUTF(int)}. Will be created
+	 * lazily in {@link #getUTF8Decoder()}
+	 */
+	private static CharsetDecoder _utf8Decoder;
+	
+	/**
+	 * @see FilterInputStream#FilterInputStream(InputStream)
+	 */
+	public LittleEndianInputStream(InputStream in) {
+		super(in);
+		_rawBuf = new byte[8];
+		_buf = ByteBuffer.wrap(_rawBuf).order(ByteOrder.LITTLE_ENDIAN);
+	}
+	
+	/**
+	 * @return the lazily created UTF-8 character set
+	 */
+	private static Charset getUTF8Charset() {
+		if (_utf8 == null) {
+			_utf8 = Charset.forName("UTF-8");
+		}
+		return _utf8;
+	}
+	
+	/**
+	 * @return the lazily created UTF-8 decoder
+	 */
+	private static CharsetDecoder getUTF8Decoder() {
+		if (_utf8Decoder == null) {
+			_utf8Decoder = getUTF8Charset().newDecoder();
+		}
+		return _utf8Decoder;
+	}
+
+	@Override
+	public void readFully(byte[] b) throws IOException {
+		readFully(b, 0, b.length);
+	}
+
+	@Override
+	public void readFully(byte[] b, int off, int len) throws IOException {
+		while (len > 0) {
+			int r = read(b, off, len);
+			if (r < 0) {
+				throw new EOFException();
+			}
+			len -= r;
+			off += r;
+		}
+	}
+
+	@Override
+	public int skipBytes(int n) throws IOException {
+		int r = 0;
+		while (n > 0) {
+			long s = skip(n);
+			if (s <= 0) {
+				break;
+			}
+			r += s;
+			n -= s;
+		}
+		return r;
+	}
+
+	@Override
+	public boolean readBoolean() throws IOException {
+		return (readByte() != 0);
+	}
+
+	@Override
+	public byte readByte() throws IOException {
+		return (byte)readUnsignedByte();
+	}
+
+	@Override
+	public int readUnsignedByte() throws IOException {
+		int r = read();
+		if (r < 0) {
+			throw new EOFException();
+		}
+		return r;
+	}
+
+	@Override
+	public short readShort() throws IOException {
+		return (short)readUnsignedShort();
+	}
+
+	@Override
+	public int readUnsignedShort() throws IOException {
+		int r1 = readUnsignedByte();
+		int r2 = readUnsignedByte();
+		return (r1 + (r2 << 8));
+	}
+
+	@Override
+	public char readChar() throws IOException {
+		return (char)readUnsignedShort();
+	}
+
+	@Override
+	public int readInt() throws IOException {
+		readFully(_rawBuf, 0, 4);
+		return _buf.getInt(0);
+	}
+
+	@Override
+	public long readLong() throws IOException {
+		readFully(_rawBuf, 0, 8);
+		return _buf.getLong(0);
+	}
+
+	@Override
+	public float readFloat() throws IOException {
+		readFully(_rawBuf, 0, 4);
+		return _buf.getFloat(0);
+	}
+
+	@Override
+	public double readDouble() throws IOException {
+		readFully(_rawBuf, 0, 8);
+		return _buf.getDouble(0);
+	}
+
+	@Override
+	public String readLine() throws IOException {
+		int bufSize = 0;
+		if (_lineBuffer != null) {
+			_lineBuffer.rewind();
+			_lineBuffer.limit(_lineBuffer.capacity());
+			bufSize = _lineBuffer.capacity();
+		}
+		
+		int c;
+		while (true) {
+			c = read();
+			if (c < 0 || c == '\n') {
+				break;
+			} else if (c == '\r') {
+				int c2 = read();
+				if (c2 != -1 && c2 != '\n') {
+					if (!(in instanceof PushbackInputStream)) {
+						in = new PushbackInputStream(in);
+					}
+					((PushbackInputStream)in).unread(c2);
+				}
+				break;
+			} else {
+				if (_lineBuffer == null || _lineBuffer.remaining() == 0) {
+					int newBufSize = bufSize + 128;
+					CharBuffer newBuf = CharBuffer.allocate(newBufSize);
+					if (_lineBuffer != null) {
+						_lineBuffer.flip();
+						newBuf.put(_lineBuffer);
+					}
+					_lineBuffer = newBuf;
+					bufSize = newBufSize;
+				}
+				_lineBuffer.put((char)c);
+			}
+		}
+		if (c < 0 && (_lineBuffer == null || _lineBuffer.position() == 0)) {
+			return null;
+		}
+		return String.valueOf(_lineBuffer.array(), 0, _lineBuffer.position());
+	}
+
+	/**
+	 * <p>Forwards to {@link DataInputStream#readUTF(DataInput)} which expects
+	 * a short value at the beginning of the UTF-8 string that specifies
+	 * the number of bytes to read.</p>
+	 * <p>If the output stream does no include such a short value, use
+	 * {@link #readUTF(int)} to explicitly specify the number of bytes.</p>
+	 */
+	@Override
+	public String readUTF() throws IOException {
+		return DataInputStream.readUTF(this);
+	}
+	
+	/**
+	 * Reads a modified UTF-8 string
+	 * @param len the number of bytes to read (please do not mix that up
+	 * with the number of characters!). If this is -1 then the method
+	 * will read bytes until the first one is zero (0x00). The zero
+	 * byte will not be included in the result string.
+	 * @return the UTF-8 string
+	 * @throws IOException if an I/O error occurs
+	 * @throws CharacterCodingException if an invalid UTF-8 character
+	 * has been read
+	 */
+	public String readUTF(int len) throws IOException {
+		return readUTF(this, len);
+	}
+	
+	/**
+	 * Reads a modified UTF-8 string from a DataInput object
+	 * @param input the DataInput object to read from
+	 * @param len the number of bytes to read (please do not mix that up
+	 * with the number of characters!). If this is -1 then the method
+	 * will read bytes until the first one is zero (0x00). The zero
+	 * byte will not be included in the result string.
+	 * @return the UTF-8 string
+	 * @throws IOException if an I/O error occurs
+	 * @throws CharacterCodingException if an invalid UTF-8 character
+	 * has been read
+	 */
+	public static String readUTF(DataInput input, int len) throws IOException {
+		ByteBuffer utf8buf;
+		byte[] rawUtf8Buf;
+
+		if (input instanceof LittleEndianInputStream) {
+			LittleEndianInputStream leis = (LittleEndianInputStream)input;
+			if (leis._utf8buf == null) {
+				leis._rawUtf8Buf = new byte[1024 * 8];
+				leis._utf8buf = ByteBuffer.wrap(leis._rawUtf8Buf);
+			} else {
+				leis._utf8buf.rewind();
+			}
+			utf8buf = leis._utf8buf;
+			rawUtf8Buf = leis._rawUtf8Buf;
+		} else {
+			rawUtf8Buf = new byte[1024 * 8];
+			utf8buf = ByteBuffer.wrap(rawUtf8Buf);
+		}
+		
+		CharsetDecoder dec = getUTF8Decoder();
+		int expectedLen = (len > 0 ? (int)(dec.averageCharsPerByte() * len) + 1 : 1024);
+		CharBuffer cb = CharBuffer.allocate(expectedLen);
+		while (len != 0 || utf8buf.position() > 0) {
+			//read as much as possible
+			if (len < 0) {
+				//read until the first zero byte
+				while (utf8buf.remaining() > 0) {
+					byte b = input.readByte();
+					if (b == 0) {
+						len = 0;
+						break;
+					}
+					utf8buf.put(b);
+				}
+				utf8buf.flip();
+			} else if (len > 0) {
+				int r = Math.min(len, utf8buf.remaining());
+				input.readFully(rawUtf8Buf, utf8buf.position(), r);
+				len -= r;
+				utf8buf.limit(r);
+				utf8buf.rewind();
+			} else {
+				utf8buf.flip();
+			}
+
+			//decode byte buffer
+			CoderResult cr = dec.decode(utf8buf, cb, len == 0);
+			if (cr.isUnderflow()) {
+				//too few input bytes. move rest of the buffer
+				//to the beginning and then try again
+				utf8buf.compact();
+			} else if (cr.isOverflow()) {
+				//output buffer to small. enlarge buffer and try again
+				utf8buf.compact();
+				CharBuffer newBuf = CharBuffer.allocate(cb.capacity() + 1024);
+				cb.flip();
+				newBuf.put(cb);
+				cb = newBuf;
+			} else if (cr.isError()) {
+				cr.throwException();
+			}
+		}
+		
+		cb.flip();
+		return cb.toString();
+	}
+}
---
+++ src/test/java/de/undercouch/bson4jackson/io/LittleEndianInputStreamTest.java
@@ -0,0 +1,186 @@
+package de.undercouch.bson4jackson.io;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.charset.CharacterCodingException;
+import java.util.Arrays;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Tests {@link LittleEndianInputStream}
+ * @author Michel Kraemer
+ */
+public class LittleEndianInputStreamTest {
+	/**
+	 * An input stream that contains some test data
+	 */
+	private ByteArrayInputStream _bais;
+	
+	/**
+	 * The input stream to test
+	 */
+	private LittleEndianInputStream _leis;
+	
+	@Before
+	public void setUp() {
+		byte[] b = new byte[] { (byte)0x66, (byte)0x77, (byte)0x88, (byte)0x99,
+				(byte)0xAA, (byte)0xBB, (byte)0xCC, (byte)0xDD, (byte)0x00, (byte)0x01 };
+		_bais = new ByteArrayInputStream(b);
+		_leis = new LittleEndianInputStream(_bais);
+	}
+	
+	@Test
+	public void readByte() throws Exception {
+		assertEquals((byte)0x66, _leis.readByte());
+	}
+	
+	@Test
+	public void readFully() throws Exception {
+		byte[] r1 = new byte[4];
+		byte[] r2 = new byte[12];
+		_leis.readFully(r1);
+		_leis.readFully(r2, 4, 4);
+		assertEquals((byte)0x66, r1[0]);
+		assertEquals((byte)0x77, r1[1]);
+		assertEquals((byte)0x88, r1[2]);
+		assertEquals((byte)0x99, r1[3]);
+		assertEquals((byte)0xAA, r2[4]);
+		assertEquals((byte)0xBB, r2[5]);
+		assertEquals((byte)0xCC, r2[6]);
+		assertEquals((byte)0xDD, r2[7]);
+	}
+	
+	@Test
+	public void skipBytes() throws Exception {
+		assertEquals(4, _leis.skip(4));
+		assertEquals((byte)0xAA, _leis.readByte());
+	}
+	
+	@Test
+	public void readBoolean() throws Exception {
+		assertEquals(8, _leis.skipBytes(8));
+		assertEquals(false, _leis.readBoolean());
+		assertEquals(true, _leis.readBoolean());
+	}
+	
+	@Test
+	public void readUnsignedByte() throws Exception {
+		assertEquals(4, _leis.skipBytes(4));
+		assertEquals(0xAA, _leis.readUnsignedByte());
+	}
+	
+	@Test
+	public void readShort() throws Exception {
+		assertEquals(4, _leis.skipBytes(4));
+		assertEquals((short)0xBBAA, _leis.readShort());
+	}
+	
+	@Test
+	public void readUnsignedShort() throws Exception {
+		assertEquals(4, _leis.skipBytes(4));
+		assertEquals(0xBBAA, _leis.readUnsignedShort());
+	}
+	
+	@Test
+	public void readChar() throws Exception {
+		assertEquals(4, _leis.skipBytes(4));
+		assertEquals((char)0xBBAA, _leis.readChar());
+	}
+	
+	@Test
+	public void readInt() throws Exception {
+		assertEquals(0x99887766, _leis.readInt());
+		assertEquals(0xDDCCBBAA, _leis.readInt());
+	}
+	
+	@Test
+	public void readLong() throws Exception {
+		assertEquals(0xDDCCBBAA99887766L, _leis.readLong());
+	}
+	
+	@Test
+	public void readFloat() throws Exception {
+		byte[] b = new byte[4];
+		ByteBuffer bb = ByteBuffer.wrap(b).order(ByteOrder.LITTLE_ENDIAN);
+		bb.putFloat(1234.1234f);
+		
+		ByteArrayInputStream bais = new ByteArrayInputStream(b);
+		LittleEndianInputStream leis = new LittleEndianInputStream(bais);
+		assertEquals(1234.1234f, leis.readFloat(), 0.00001);
+	}
+	
+	@Test
+	public void readDouble() throws Exception {
+		byte[] b = new byte[8];
+		ByteBuffer bb = ByteBuffer.wrap(b).order(ByteOrder.LITTLE_ENDIAN);
+		bb.putDouble(1234.1234);
+		
+		ByteArrayInputStream bais = new ByteArrayInputStream(b);
+		LittleEndianInputStream leis = new LittleEndianInputStream(bais);
+		assertEquals(1234.1234, leis.readDouble(), 0.00001);
+	}
+	
+	@Test
+	public void readLine() throws Exception {
+		byte[] b = new byte[] { 'H', 'e', 'l', 'l', 'o', '\r',
+				'W', 'o', 'r', 'l', 'd', '\r', '\n' };
+		ByteArrayInputStream bais = new ByteArrayInputStream(b);
+		LittleEndianInputStream leis = new LittleEndianInputStream(bais);
+		assertEquals("Hello", leis.readLine());
+		assertEquals("World", leis.readLine());
+		assertNull(leis.readLine());
+		
+		byte[] b2 = new byte[1024 * 8];
+		Arrays.fill(b2, (byte)'a');
+		bais = new ByteArrayInputStream(b2);
+		leis = new LittleEndianInputStream(bais);
+		String s = leis.readLine();
+		assertNotNull(s);
+		byte[] line = s.getBytes();
+		assertEquals(b2.length, line.length);
+		for (int i = 0; i < b2.length; ++i) {
+			assertEquals(b2[i], line[i]);
+		}
+	}
+	
+	private LittleEndianInputStream outputBufferToInputStream(DynamicOutputBuffer db) throws Exception {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		db.writeTo(baos);
+		ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
+		return new LittleEndianInputStream(bais);
+	}
+	
+	@Test
+	public void readUTF() throws Exception {
+		DynamicOutputBuffer db = new DynamicOutputBuffer(20);
+		db.putUTF8("Helloa\u20AC\u00A2\u00A2bb");
+		LittleEndianInputStream leis = outputBufferToInputStream(db);
+		assertEquals("Hello", leis.readUTF(5));
+		assertEquals("a\u20AC\u00A2\u00A2bb", leis.readUTF(db.size() - 5));
+	}
+	
+	@Test(expected = CharacterCodingException.class)
+	public void readUTFError() throws Exception {
+		DynamicOutputBuffer db = new DynamicOutputBuffer(20);
+		db.putUTF8("a\u00A2");
+		LittleEndianInputStream leis = outputBufferToInputStream(db);
+		leis.readUTF(2);
+	}
+	
+	@Test
+	public void readZeroTerminatedUTF() throws Exception {
+		DynamicOutputBuffer db = new DynamicOutputBuffer(20);
+		db.putUTF8("Hello");
+		db.putByte((byte)0);
+		LittleEndianInputStream leis = outputBufferToInputStream(db);
+		assertEquals("Hello", leis.readUTF(-1));
+	}
+}
